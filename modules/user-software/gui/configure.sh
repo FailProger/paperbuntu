17ed5d0ee8ee4954cd7a02c2348af50ef0d7c807
FailProger <WsrrcalzWehgwmD@protonmail.com>
2026-08-25 13:24:26 +0300

    feat: add compinit and fix bug with zoxide
    Zoxide `Space + Tab` do not work with zicompinit. Zicompinit be change
    to default `autoload -Uz compinit`.
    
    Also:
     - Remove aliases for `john` and `dirsearch` in `.zsh_custom`.
     - Add `john` and `dirsearch` bash scripts to `/usr/local/bin` for
       normal tools run.
     - Also change SecLists dir to `/usr/local/share/seclists`.
     - Add symlink for `rockyou.txt` to `/usr/local/share/wordlists`.

--- modules/user-software/gui/configure.sh
+++ modules/user-software/gui/configure.sh
@@ -3,11 +3,12 @@ source "$ROOT_DIR/lib/user.sh"
 
 user_software_gui__configure_all() {
   # Configure all gui programms
-  _user_software_gui__configure_alacritty
+  _configure_alacritty
 }
 
-_user_software_gui__configure_alacritty() {
+_configure_alacritty() {
   # Configure alacritty
   cp_config 'alacritty'
   update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator "$(which alacritty)" 100
 }
+
